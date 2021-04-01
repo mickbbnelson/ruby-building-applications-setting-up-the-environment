55c86348730bab3f57ce5140fb9b884d54b423e4
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-03-31 17:21:04 -0700

    commit tree 910de7d252e301e26034122a10d01eb118aec32c

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@ require_relative '../lib/garden.rb'
 require_relative '../lib/plant.rb'
 
 # Require statements should go here.
+require 'bundler/setup'
+Bundler.require(:default, :development) 
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
